7e15b2ce80ae5f43c1a41f58a0db225c88681550
Ralf Konusch <rk@wobe-systems.com>
2016-09-12 13:24:07 +0200

    Suppress unused expressions in CMakeLists.txt

--- run_test.sh
+++ run_test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-rm ./test/test_01
+rm -f ./test/test_01
 cp build/bin/test_01 ./test/test_01
 cd test
 ./test_01
--- run_test_freeBSD32.sh
+++ run_test_freeBSD32.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-rm ./test/test_01
+rm -f ./test/test_01
 cp ./build/freeBSD_x86-32/test/test_01 ./test
 cd ./test
 ./test_01
